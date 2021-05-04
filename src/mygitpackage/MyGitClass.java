c2323f2700fd0636d597d49d61c57cda131c173a
Orhan98 <ismailorhandogan@gmail.com>
2021-05-04 16:30:00 +0200

    First try

---
+++ src/mygitpackage/MyGitClass.java
@@ -0,0 +1,21 @@
+package mygitpackage;
+
+public class MyGitClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Version 1");
+		System.out.println("Version 1, my second commit.");
+		System.out.println("My third commit.");
+		System.out.println("My fourth commit");
+		System.out.println("My fifth commit.");
+		System.out.println("Githubta yazdim ");
+		System.out.println("Tekrar githubta yazdim");
+		System.out.println("ismail branchi");
+		System.out.println("Githubtan merhaba");
+		System.out.println("Deneme 345");
+		System.out.println("Yeter  calis artik");
+
+	}
+
+}
